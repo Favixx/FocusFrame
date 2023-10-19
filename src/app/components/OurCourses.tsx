ca53ff8c06d20c12fcdbfbc7d595e7adda91e223
Nikita <bazilik2452@gmail.com>
2023-10-19 20:33:30 +0200

    our_courses section

--- src/app/components/OurCourses.tsx
+++ src/app/components/OurCourses.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from "react"
 
 const OurCourses = () => {
   return (
-    <div>OurCourses</div>
+    <section className="flex flex-col m-auto mb-10">
+      <h2 className="font-medium text-[28px] leading-[32px] tracking-[-1.12px] mb-5">
+        Our{" "}
+        <span className="text-[#BEA69E] font-medium text-[28px] leading-[32px] tracking-[-1.12px]">
+          Courses
+        </span>
+      </h2>
+      <p className="text-[14px] leading-[18px] tracking-[-0.28px]">
+        At our photography school, we offer a variety of courses that cover all aspects of this
+        fascinating art. Regardless of your experience or skill level, we have courses to suit your
+        needs and goals.
+      </p>
+    </section>
   )
 }
 
